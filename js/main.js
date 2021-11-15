f6f6c79f9f9494a8f950a6afcfa0e75a7220ee85
Aguirre Fabian <aguirrefabian.unne@gmail.com>
2021-11-14 23:11:05 -0300

    a new portfolio website

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.typed', {    
+    stringsElement: '#text', 
+    typeSpeed: 50, 
+    startDelay: 300, 
+    backSpeed: 50, 
+    smartBackspace: true, 
+    shuffle: false, 
+    backDelay: 1500, 
+    loop: true,
+    loopCount: false, 
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
+
+
+
+
